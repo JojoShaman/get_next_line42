dc715c3f9356ed20f7c3cfdc16553f4498e9958b
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-04-22 01:02:13 +0200

    .h and utils functions

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   get_next_line.h                                   :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/04/22 00:13:27 by srosu            #+#    #+#              */
+/*   Updated: 2026/04/22 01:00:52 by srosu           ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 7
+# endif
+# if BUFFER_SIZE <= 0 || BUFFER_SIZE > 0x7ffff000
+#  error "Invalid buffer size"
+# endif
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(char *str);
+char	*ft_substr(char *str, size_t start, size_t size);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   get_next_line_utils.c                             :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/04/22 00:14:28 by srosu            #+#    #+#              */
+/*   Updated: 2026/04/22 01:00:50 by srosu           ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*new_str;
+	char	*ptr;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new_str = malloc(((s1_len + s2_len) + 1) * sizeof(*new_str));
+	if (!new_str)
+		return (NULL);
+	ptr = new_str;
+	while (*s1)
+		*new_str++ = *s1++;
+	while (*s2)
+		*new_str++ = *s2++;
+	*new_str = '\0';
+	return (ptr);
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*copy;
+	char	*ptr;
+
+	if (!str)
+		return (NULL);
+	copy = malloc((ft_strlen(str) + 1) * sizeof(*copy));
+	if (!copy)
+		return (NULL);
+	ptr = copy;
+	while (*str)
+		*copy++ = *str++;
+	*copy = '\0';
+	return (ptr);
+}
+
+char	*ft_substr(char *str, size_t start, size_t size)
+{
+	char	*new_str;
+	char	*ptr;
+
+	if (!str)
+		return (NULL);
+	if (start >= ft_strlen(str))
+		return (ft_strdup(""));
+	new_str = malloc(size + 1);
+	if (!new_str)
+		return (NULL);
+	ptr = new_str;
+	while (size-- && str[start])
+		*new_str++ = str[start++];
+	*new_str = '\0';
+	return (ptr);
+}
